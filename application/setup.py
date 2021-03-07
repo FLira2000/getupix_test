272c0037bf4c15e711f7a258cb42550d862da559
FLira2000 <fabioliradev@gmail.com>
2021-03-07 08:06:37 -0300

    smol file to start the server

--- application/setup.py
+++ application/setup.py
@@ -1,13 +1,5 @@
-import pymongo
-from pymongo import MongoClient
+from database import create_collection
 
-def initialize_collection():
-    collection = 0
-    try:
-        cluster = MongoClient('mongodb+srv://root:root@owid-covid-cluster.ncb4c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-        db = cluster['owid-covid-cluster']  
-        collection = db['covid_timeseries']
-    except:
-        print('Erro de servidor.')
-
-    return collection
+print('Esse processo deve demorar um pouco. Por favor, aguarde.')
+print('.\n'*4)
+create_collection()
