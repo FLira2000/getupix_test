8bcc38d370fc7fa4a4ef6451c67582dfcf3f459b
FLira2000 <fabioliradev@gmail.com>
2021-03-07 04:34:45 -0300

    insert one

--- application/routes.py
+++ application/routes.py
@@ -1,15 +1,24 @@
-from flask import Flask, request
+from typing import Type
+from flask import Flask, request, Response
 from remote import find_one, insert_one
+import json
 
 app = Flask("owid_covid")
 
 @app.route("/", methods=["GET"])
 def index():
-    return {"Flask": "Just Flask"}
+    return {"Home": "Sweet Home"}
 
 @app.route("/insert", methods=['POST'])
 def insert():
-    body = request.get_json()
-    insert_one(body)
-    
+    try:
+        body = request.get_json()
+        dict_body = json.loads(json.dumps(body))
+
+        insert_one(dict_body)
+    except:
+        return Response("Invalid JSON", status=400)
+
+    return "OK"
+
 app.run()
